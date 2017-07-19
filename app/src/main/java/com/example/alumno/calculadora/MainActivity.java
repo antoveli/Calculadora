34cb54b0922d2a6a710fe971dfb179b8efcc843e
U. Mariano Gálvez <alumno@labs.academico>
2017-07-18 19:58:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/alumno/calculadora/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.alumno.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1, et2;
+    private Button bt1;
+    private TextView tv1;
+    private RadioButton rb1, rb2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        et1= (EditText) findViewById(R.id.et1);
+        et2 = (EditText) findViewById(R.id.et2);
+        tv1 = (TextView) findViewById(R.id.tv1);
+
+        rb1 = (RadioButton) findViewById(R.id.rb1);
+        rb2 = (RadioButton) findViewById(R.id.rb2);
+    }
+
+    public void sumar(View view)
+    {
+        int numero1 = Integer.parseInt(et1.getText().toString());
+        int numero2 = Integer.parseInt(et2.getText().toString());
+        //int suma = Integer.parseInt(et1.getText().toString()) + Integer.parseInt(et2.getText().toString()) ;
+        tv1.setText(numero1+numero2);
+    }
+
+    public void calcular(View view)
+    {
+        int numero1 = Integer.parseInt(et1.getText().toString());
+        int numero2 = Integer.parseInt(et2.getText().toString());
+
+        if(rb1.isChecked())
+        {
+          tv1.setText(String.valueOf (numero1+numero2));
+        }
+        else
+            {
+                tv1.setText(String.valueOf (numero1-numero2));
+        }
+    }
+}
